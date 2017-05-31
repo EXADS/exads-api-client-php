caecb0ae8529fe074ba9cc5c457fe8c4051e27bd
Alfred Dobradi <alfred@exads.com>
2017-05-31 15:47:06 +0100

    Retrieve all offers

---
+++ lib/Exads/Api/Offer.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Exads\Api;
+
+/**
+ * @link   https://api.exads.com/v1/docs/index.html#!/offers
+ */
+class Offer extends AbstractApi
+{
+    protected $apiGroup = 'offers';
+
+    /**
+     * @param array $params
+     *
+     * @return array
+     */
+    public function all(array $params = array())
+    {
+        return $this->get($this->getPath(), $params);
+    }
+}
